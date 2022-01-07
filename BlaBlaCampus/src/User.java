396fdf0dd74d9ea4dfce466288cbf31376fadfbf
leurs-eloise <efml74@gmail.com>
2022-01-07 08:59:18 +0100

    ok

--- BlaBlaCampus/src/User.java
+++ BlaBlaCampus/src/User.java
@@ -1,4 +1,11 @@
 
 public class User {
+	private String prenom;
+	private String adresse;
+	private int id_user;
+	private String mdp;
+	private boolean estConnecte;
+	private Behavior drivingBehavior;
+
 
 }
