a0b902be266251886f678738a8c79bc2bf995402
Antoine <antoine.roth@etu.univ.smb.fr>
2022-01-07 09:48:04 +0100

    messages

--- BlaBlaCampus/src/MessageEvent.java
+++ BlaBlaCampus/src/MessageEvent.java
@@ -1,4 +1,20 @@
+import java.util.EventObject;
 
-public class MessageEvent {
+public class MessageEvent extends EventObject {
+	private String contenu;
+
+	public MessageEvent(Object source, String contenu) {
+		super(source);
+		// TODO Auto-generated constructor stub
+		this.contenu = contenu;
+	}
+
+	public String getContenu() {
+		return contenu;
+	}
+
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
 
 }
