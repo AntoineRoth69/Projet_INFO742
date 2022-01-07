d8e035d32752eb8b3346dc225d621902c792ab34
leurs-eloise <efml74@gmail.com>
2022-01-07 09:38:43 +0100

    mise à jour résa + trajet

--- BlaBlaCampus/src/Trajet.java
+++ BlaBlaCampus/src/Trajet.java
@@ -1,16 +1,21 @@
+import java.util.ArrayList;
 
 public class Trajet {
+	private int id_trajet;
+	private int id_1 = 0;
 	private String adresse_depart;
 	private String date;
 	private String adresse_arrivee;
 	private int prix;
 	private String horaire;
 	private int nbPlacesDispo;
+	private ArrayList <Reservation>listeReservation;
 	
 	
 	public Trajet(String adresse_depart, String date, String adresse_arrivee, int prix, String horaire,
 			int nbPlacesDispo) {
-		super();
+		this.id_trajet = id_1;
+		this.id_1 = id_1++;
 		this.adresse_depart = adresse_depart;
 		this.date = date;
 		this.adresse_arrivee = adresse_arrivee;
@@ -19,7 +24,10 @@ public class Trajet {
 		this.nbPlacesDispo = nbPlacesDispo;
 	}
 
-
+	public void updateListe(Reservation resa) {
+		this.listeReservation.add(resa);
+	}
+	
 	public void updateNbPlaces(int nb_places) {
 		this.nbPlacesDispo = nbPlacesDispo - nb_places;
 	}
