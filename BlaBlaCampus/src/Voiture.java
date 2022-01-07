1dd9baa8aac9730be61931c582d8df89fd236d29
Clem <clemencedessoliers@outlook.fr>
2022-01-07 09:19:22 +0100

    getter and setter de voiture

--- BlaBlaCampus/src/Voiture.java
+++ BlaBlaCampus/src/Voiture.java
@@ -1,4 +1,26 @@
 
 public class Voiture {
+	private String marque;
+	private String modele;
+	private String couleur;
+	
+	public String getMarque() {
+		return marque;
+	}
+	public void setMarque(String marque) {
+		this.marque = marque;
+	}
+	public String getModele() {
+		return modele;
+	}
+	public void setModele(String modele) {
+		this.modele = modele;
+	}
+	public String getCouleur() {
+		return couleur;
+	}
+	public void setCouleur(String couleur) {
+		this.couleur = couleur;
+	}
 
 }
