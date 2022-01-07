b21d2b07fbd1bb60472d561a927c77d5fa863b7a
leurs-eloise <efml74@gmail.com>
2022-01-07 09:18:15 +0100

    getter setter de trajet

--- BlaBlaCampus/src/Trajet.java
+++ BlaBlaCampus/src/Trajet.java
@@ -1,4 +1,86 @@
 
 public class Trajet {
+	private String adresse_depart;
+	private String date;
+	private String adresse_arrivee;
+	private int prix;
+	private String horaire;
+	private int nbPlacesDispo;
+	
+	
+	public Trajet(String adresse_depart, String date, String adresse_arrivee, int prix, String horaire,
+			int nbPlacesDispo) {
+		super();
+		this.adresse_depart = adresse_depart;
+		this.date = date;
+		this.adresse_arrivee = adresse_arrivee;
+		this.prix = prix;
+		this.horaire = horaire;
+		this.nbPlacesDispo = nbPlacesDispo;
+	}
+
+
+	public void updateNbPlaces(int nb_places) {
+		this.nbPlacesDispo = nbPlacesDispo - nb_places;
+	}
+
+	
+	//Getter et setter
+	public String getAdresse_depart() {
+		return adresse_depart;
+	}
+
+
+	public void setAdresse_depart(String adresse_depart) {
+		this.adresse_depart = adresse_depart;
+	}
+
+
+	public String getDate() {
+		return date;
+	}
+
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+
+	public String getAdresse_arrivee() {
+		return adresse_arrivee;
+	}
+
+
+	public void setAdresse_arrivee(String adresse_arrivee) {
+		this.adresse_arrivee = adresse_arrivee;
+	}
+
+
+	public int getPrix() {
+		return prix;
+	}
+
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
+
+	public String getHoraire() {
+		return horaire;
+	}
+
+
+	public void setHoraire(String horaire) {
+		this.horaire = horaire;
+	}
+
+
+	public int getNbPlacesDispo() {
+		return nbPlacesDispo;
+	}
+	
+	
+	
 
 }
--- BlaBlaCampus/src/User.java
+++ BlaBlaCampus/src/User.java
@@ -1,11 +1,56 @@
 
 public class User {
+	private int id_user;
 	private String prenom;
 	private String adresse;
-	private int id_user;
 	private String mdp;
 	private boolean estConnecte;
 	private Behavior drivingBehavior;
+	
+	public User (int id_user, String prenom) {
+		this.prenom = prenom;
+		this.id_user = id_user;
+	}
 
+	
+	
+	//Getter et setter
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public boolean isEstConnecte() {
+		return estConnecte;
+	}
+
+	public void setEstConnecte(boolean estConnecte) {
+		this.estConnecte = estConnecte;
+	}
+
+	public Behavior getDrivingBehavior() {
+		return drivingBehavior;
+	}
+
+	public void setDrivingBehavior(Behavior drivingBehavior) {
+		this.drivingBehavior = drivingBehavior;
+	}
+
+	public int getId_user() {
+		return id_user;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+	
+	
 
 }
