a037f84a2d06c81fac9e4c0fc44249cfeb52cc45
Clem <clemencedessoliers@outlook.fr>
2022-01-10 11:07:10 +0100

    Comment behavior

--- BlaBlaCampus/src/BadBehavior.java
+++ BlaBlaCampus/src/BadBehavior.java
@@ -5,6 +5,12 @@ public class BadBehavior implements Behavior{
 		
 	}
 
+	/**
+	 *@param prix_trajet
+	 *@param o
+	 *@return float 
+	 * Permet d'affecter le desavantage aux passagers mal note, il paye 5% plus chere
+	 */
 	@Override
 	public float getAvantage(float prix_trajet, Object o) {
 		float res = 0;
